82a776a3110fda95880d2bf37c8f7240194a7fe7
soponwich087 <123313612+soponwich087@users.noreply.github.com>
2023-09-11 10:20:23 +0000

    6531503087_lab4

--- src/main/java/th/mfu/Concert.java
+++ src/main/java/th/mfu/Concert.java
@@ -4,23 +4,62 @@ import java.util.Date;
 
 public class Concert {
 
-    //TODO: add attributes
+    // TODO: add attributes
+    private int id;
+    private String title;
+    private String performer;
+    private Date date;
+    private String description;
 
     public Concert() {
     }
+
     public Concert(String title, String description) {
-        //TODO: set attributes
+        // TODO: set attributes
+        this.title = title;
+        this.description = description;
     }
- 
+
     public String getTitle() {
-        //TODO: return title
-        return "";
+        // TODO: return title
+        return title;
     }
+
     public String getDescription() {
-        //TODO: return description
-        return "";
+        // TODO: return description
+        return description;
     }
 
-    //TODO: add getters and setters
+    // TODO: add getters and setters
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPerformer() {
+        return performer;
+    }
+
+    public void setPerformer(String performer) {
+        this.performer = performer;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- src/main/java/th/mfu/ConcertController.java
+++ src/main/java/th/mfu/ConcertController.java
@@ -20,44 +20,60 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class ConcertController {
     // TODO: create hashmap of concerts for storing data
+    private HashMap<Integer, Concert> concerts = new HashMap<Integer, Concert>();
+    private int nextId = 1; // To generate unique IDs for concerts
 
-    //TODO: add initbinder to convert date
+    // Initialize a date format for date input
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+
+    // TODO: add initbinder to convert date
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+    }
 
     @GetMapping("/concerts")
     public String listConcerts(Model model) {
         // TODO: add concerts to model
+        model.addAttribute("concerts", concerts.values());
         // TODO: return a template to list concerts
-        return "";
+        return "list-concert";
     }
 
     @GetMapping("/add-concert")
     public String addAConcertForm(Model model) {
         // TODO: pass blank concert to a form
+        model.addAttribute("concerts", new Concert());
         // TODO: return a template for concert form
-        return "";
+        return "add-concert-form";
     }
 
     @PostMapping("/concerts")
     public String saveConcert(@ModelAttribute Concert concert) {
         // TODO: add concert to list of concerts
+        concert.setId(nextId);
+        concerts.put(nextId, concert);
         // TODO: increment nextId
+        nextId++;
         // TODO: redirect to list concerts
-        return "";
+        return "redirect:/concerts";
     }
 
     @GetMapping("/delete-concert/{id}")
     public String deleteConcert(@PathVariable int id) {
         // TODO: remove concert from list of concerts
+        concerts.remove(id);
         // TODO: redirect to list concerts
-        return "";
+        return "redirect:/concerts";
     }
 
-    
     @GetMapping("/delete-concert")
     public String removeAllConcerts() {
-        //TODO: clear all employees and reset id
+        // TODO: clear all employees and reset id
+        concerts.clear();
+        nextId = 1;
         // TODO: redirect to list concerts
-        return "";
+        return "redirect:/concerts";
     }
 
 }
